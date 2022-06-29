8b141fe9a448007ede13e3523e49cff6d94b4dfc
Isaac Creer <isaaccreer@gmail.com>
2022-06-28 22:27:41 -0600

    More update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,8 +8,10 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     //println("Program arguments: ${args.joinToString()}")
     val id = Main().welcome()
-    var AddToTotal = true
+
     var havePaid = true
+    var CTotal = 0.0
+    var paytype = 0
     while (havePaid){
         println("Please tell me the type of item you want to buy: \n"
         + " 1. Gas \n"
@@ -22,25 +24,29 @@ fun main(args: Array<String>) {
             havePaid = false
         }
         else if (customizer == "1"){
-            Items().gas(id, AddToTotal)
+            Items().gas(id)
         }
         else if (customizer == "2"){
-            Items().food(id, AddToTotal)
+            Items().food(id)
         }
         else if (customizer == "3"){
-            Items().CarProduce(id, AddToTotal)
+            Items().CarProduce(id)
         }
         else if (customizer == "4"){
-            Items().other(id, AddToTotal)
+            Items().other(id)
         }
         else{
             println("Sorry that is not an option")
         }
 
     }
+    CTotal = Receipt().showtotal(id)
+    paytype= Receipt().paymenttype(id)
+
+    Receipt().printreipt(id,CTotal,paytype)
 }
 
-data class tranzaction( val userID : String ){
+data class Tranzaction( val userID : String ){
     var total : Double = 0.0
     var itemtotal : Double = 0.0
     val itemscost: MutableList<Double> = mutableListOf()
@@ -62,10 +68,10 @@ fun rand(from: Double, to: Double) : Double{
 }
 
 class Items(){
-    fun gas( userid: String, AD: Boolean) : Boolean{
+    fun gas( userid: String) : Boolean{
         println("Please enter the pump you are on: ")
         val pumpnumber = readLine().toString()
-        tranzaction(userid).item.add("Gass")
+        Tranzaction(userid).item.add("Gass")
         println("Please select an option how much you want to pay: "
                 + "1. A fix amount\n"
                 + "2. The remader after buying someother items ")
@@ -73,9 +79,9 @@ class Items(){
         if (gas_option ==  1){
             println("Please enter the amount of gas you want to buy: ")
             val gasamount = readLine().toString().toDouble()
-            tranzaction(userid).itemscost.add(gasamount)
+            Tranzaction(userid).itemscost.add(gasamount)
             println("You are putting " + gasamount + " to pump " + pumpnumber)
-            tranzaction(userid).itemtotal = tranzaction(userid).itemtotal + gasamount + 1
+            Tranzaction(userid).itemtotal = Tranzaction(userid).itemtotal + gasamount + 1
             return true
         }
         else if (gas_option == 2){
@@ -83,7 +89,7 @@ class Items(){
             val spendamount = readLine().toString().toDouble()
             println("Please keep on shopping as long as you stay under the amount you enter" +
                     "you will be told the amount of gas you can pump into pump number" + pumpnumber )
-            tranzaction(userid).total = spendamount
+            Tranzaction(userid).total = spendamount
             return false
         }
         else{
@@ -92,43 +98,64 @@ class Items(){
         }
     }
 
-    fun food( userid:String, AD: Boolean){
+    fun food( userid:String){
         println("Please enter in the food you are getting: ")
         val items = readLine().toString()
-        tranzaction(userid).item.add(items)
+        Tranzaction(userid).item.add(items)
         val cost = rand(0.01,10.0)
         println("The cost for " + items + " is " + cost)
-        tranzaction(userid).itemscost.add(cost)
-        tranzaction(userid).itemtotal = tranzaction(userid).itemtotal + (cost * 1.06)
+        Tranzaction(userid).itemscost.add(cost)
+        Tranzaction(userid).itemtotal = Tranzaction(userid).itemtotal + (cost * 1.06)
     }
-    fun CarProduce( userid: String, AD:Boolean){
+    fun CarProduce( userid: String){
         println("Please enter in the Car item you are getting: ")
         val item = readLine().toString()
-        tranzaction(userid).item.add(item)
+        Tranzaction(userid).item.add(item)
         val cost = rand(10.01,100.0)
         println("The cost for " + item + " is " + cost)
-        tranzaction(userid).itemscost.add(cost)
-        tranzaction(userid).total = tranzaction(userid).total + (cost * 1.10)
+        Tranzaction(userid).itemscost.add(cost)
+        Tranzaction(userid).itemtotal = Tranzaction(userid).itemtotal + (cost * 1.10)
     }
-    fun other (userid:String, AD:Boolean){
+    fun other (userid:String){
         println("Please enter in the item you are getting: ")
         val item = readLine().toString()
-        tranzaction(userid).item.add(item)
+        Tranzaction(userid).item.add(item)
         val cost = rand(0.01,1000.0)
         println("The cost for " + item + " is " + cost)
-        tranzaction(userid).itemscost.add(cost)
-        tranzaction(userid).total = tranzaction(userid).total + (cost * 1.15)
+        Tranzaction(userid).itemscost.add(cost)
+        Tranzaction(userid).itemtotal = Tranzaction(userid).itemtotal + (cost * 1.15)
     }
 }
 
 class Receipt(){
-    fun showtotal(){
+    fun showtotal(userid: String) :Double{
+       val totals = Tranzaction(userid).total
+       val itemtotals = Tranzaction(userid).itemtotal
+        if (totals == 0.0){
+            return itemtotals
+        }
+        else{
+            if (totals < itemtotals) {
+                println("You will not be able to get gas unless you add more to prepay")
+                return 0.0
+            }
+            else {
+                println("That amount of gas you are get is $" + (totals - itemtotals))
+                return totals
+            }
+        }
+    }
+    fun paymenttype( userid: String) :Int{
+        println("Please select your payment type:\n"
+                + "1. Credit / Debit\n"
+                + "2. Cash\n"
+                + "3. Gift Card\n"
+                + "4. Check\n")
+        val payt = readLine().toString().toInt()
+        return payt
 
     }
-    fun paymenttype(){
-
-    }
-    fun printreipt(){
+    fun printreipt(userid: String, amount: Double, paytype: Int){
 
     }
 
